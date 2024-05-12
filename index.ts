283947d5656e33c15c1849126d7fb49faf96e15d
Aether Chen <15167799+chenjunyu19@users.noreply.github.com>
2024-05-12 11:44:03 +0800

    🐛 use assgin instead of replace

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
 import { Context } from 'hydrooj';
 
+function assign(dest, src) {
+    for (const prop in src) {
+        if (!(prop in dest)) {
+            dest[prop] = src[prop];
+        }
+    }
+}
+
 export async function apply(ctx: Context) {
     ctx.on('handler/before', (h) => {
         if (h.request.headers['x-hydro-encoding'] === 'base64') {
             const payload = JSON.parse(Buffer.from(h.request.body.encodedBody, 'base64').toString());
-            h.request.body = { ...payload, ...h.request.body };
-            h.args = { ...payload, ...h.args };
+            assign(h.request.body, payload);
+            assign(h.args, payload);
         }
     });
 }
